62185dc31f11bdf2c0e616a9c43a9fc91aa8f540
naxtox <62326655+naxtox@users.noreply.github.com>
2021-02-28 08:50:14 +0700

    Update and rename README.md to Exercise 4 _ohm_.py

---
+++ Exercise 4 _ohm_.py
@@ -0,0 +1,10 @@
+eng = 2.3
+busi = 2.00
+comSys = 2.1
+comPro = 3.00
+
+print("----Your Grade----")
+print("Foundation English :", eng)
+print("General Business :", busi)
+print("Introduction to Computer Systems :", comSys)
+print("Computer Programming :", comPro)
